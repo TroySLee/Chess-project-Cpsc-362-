175c30e24a3b98f0c7eb930ea359b3d0f9e1facd
csuf-perez <144319344+csuf-perez@users.noreply.github.com>
2023-09-14 00:09:25 -0700

    Add functions that need to be implemented
    makeMove() and getValidMove() are the basic functions we need to get the game working.
    
    It would be nice to see available paths for individual pieces when said piece is selected, thus getPiecePath() is needed.

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,13 +1,27 @@
-class GameState():
+import pygame as p
+
+class GameState:
     def __init__(self):
         self.board = [
-            ["bRook", "bKnight", "bBishop", "bQueen", "bKing", "bBishop", "bKnight", "bRook"],
-            ["bPawn", "bPawn", "bPawn", "bPawn", "bPawn", "bPawn", "bPawn", "bPawn"],
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["wPawn", "wPawn", "wPawn", "wPawn", "wPawn", "wPawn", "wPawn", "wPawn"],
-            ["wRook", "wKnight", "wBishop", "wQueen", "wKing", "wBishop", "wKnight", "wRook"]]
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
         self.whiteToMove = True
         self.moveLog = []
+
+    def makeMove():
+	"""This function is going to move the chess pieces on the backend board"""
+
+    def getValidMoves():
+	"""This function is going to check that the move requested by player is legal"""
+
+    def getPiecePath(self):
+	"""Get available path for piece selected"""
+
+  
